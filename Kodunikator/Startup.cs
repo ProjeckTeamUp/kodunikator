dba187044bca17840c4f1a63d061c5ddeb940a1a
pepperoni666 <peretser08@gmail.com>
2018-10-23 22:12:27 +0200

    Add project files.

---
+++ Kodunikator/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Kodunikator.Startup))]
+
+namespace Kodunikator
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var config = new Microsoft.AspNet.SignalR.HubConfiguration();
+			config.EnableJSONP = true;
+
+			app.MapSignalR(config);
+		}
+	}
+}
